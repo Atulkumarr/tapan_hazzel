65ee73cabbc6a932b5849991ac8af1f57a5852ec
Atul Kumar <atul170301@gmail.com>
2022-06-18 11:54:56 +0530

    tapan files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  autoplay: false,
+  autoplayTimeout: 1520,
+  smartSpeed: 1500,
+  animateIn: "linear",
+  animateOut: "linear",
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
+
+AOS.init();
